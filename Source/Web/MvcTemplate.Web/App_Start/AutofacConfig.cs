1a0ff4f87b89846ef80987765faf76123424b41b
Nikolay Kostov <admin@nikolay.it>
2016-02-09 18:04:38 +0200

    Integrated Autofac as dependency container

---
+++ Source/Web/MvcTemplate.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,49 @@
+﻿namespace MvcTemplate.Web
+{
+    using System.Data.Entity;
+    using System.Diagnostics;
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using Autofac;
+    using Autofac.Integration.Mvc;
+
+    using MvcTemplate.Data;
+
+    // http://docs.autofac.org/en/latest/integration/mvc.html
+    public static class AutofacConfig
+    {
+        public static void RegisterAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // OPTIONAL: Register model binders that require DI.
+            builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinderProvider();
+
+            // OPTIONAL: Register web abstractions like HttpContextBase.
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // OPTIONAL: Enable property injection in view pages.
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // OPTIONAL: Enable property injection into action filters.
+            builder.RegisterFilterProvider();
+
+            // Register services
+            RegisterServices(builder);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        public static void RegisterServices(ContainerBuilder builder)
+        {
+            builder.Register(c => new ApplicationDbContext()).As<DbContext>().InstancePerRequest();
+        }
+    }
+}
